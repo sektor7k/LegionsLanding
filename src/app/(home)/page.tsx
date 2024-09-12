cb415e5280f9f77017b945c33446f3cffcd2a32d
sektor7k <omeraydin2112@gmail.com>
2024-09-12 23:41:47 +0300

    add mail route and client

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { LampContainer } from "@/components/ui/lamp";
 import Maskot from "@/components/ThreeModel/Maskot";
@@ -17,6 +17,8 @@ import {
   IconTerminal2,
 } from "@tabler/icons-react";
 import Image from "next/image";
+import axios from 'axios'
+import { toast } from "@/components/ui/use-toast";
 
 export default function Page() {
   const placeholders = [
@@ -26,77 +28,103 @@ export default function Page() {
     "For answers to all these questions, please join the waitlist.",
   ];
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
-  };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("submitted");
-  };
+  const [email, setMail] = useState("");
 
-  const links = [
-    
-    {
-      title: "Twitter",
-      icon: (
-        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "https://x.com/CastrumLegion",
-    },
-    {
-      title: "Discord",
-      icon: (
-        <IconBrandDiscord className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "https://discord.gg/mSxS8xJJ",
-    },
- 
-    {
-      title: "Telegram",
-      icon: (
-        <IconBrandTelegram className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "https://t.me/CastrumLegions/1",
-    },
-    {
-      title: "Youtube",
-      icon: (
-        <IconBrandYoutube className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "https://www.youtube.com/@castrumlegions",
-    },
-    {
-      title: "GitHub",
-      icon: (
-        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-  ];
+  function showErrorToast(message: string): void {
+    toast({
+      variant: "destructive",
+      title: message,
+      description: "",
+    })
+  }
 
-  return (
-    <div className=" w-full bg-black bg-grid-white/[0.2] relative flex flex-col items-center justify-center h-screen">
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <div className="transform -translate-y-16">
-      <FloatingDock
-        mobileClassName="translate-y-20" 
-        items={links}
-      />
+  function showToast(message: string): void {
+    toast({
+      variant: "default",
+      title: message,
+      description: "",
+    })
+  }
+
+
+  const onSubmit = async () => {
+    try {
+
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_APP_URL}/api/addmail`, { email })
+      console.log("Signup success", response);
+      showToast("Successfully!");
+
+    } catch (error: any) {
+      const errorMessage = error.response.data.message;
+      showErrorToast(errorMessage);
+    };
+  }
+
+   
+
+    const links = [
+
+      {
+        title: "Twitter",
+        icon: (
+          <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+        ),
+        href: "https://x.com/CastrumLegion",
+      },
+      {
+        title: "Discord",
+        icon: (
+          <IconBrandDiscord className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+        ),
+        href: "https://discord.gg/mSxS8xJJ",
+      },
+
+      {
+        title: "Telegram",
+        icon: (
+          <IconBrandTelegram className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+        ),
+        href: "https://t.me/CastrumLegions/1",
+      },
+      {
+        title: "Youtube",
+        icon: (
+          <IconBrandYoutube className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+        ),
+        href: "https://www.youtube.com/@castrumlegions",
+      },
+      {
+        title: "GitHub",
+        icon: (
+          <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+        ),
+        href: "#",
+      },
+    ];
+
+    return (
+      <div className=" w-full bg-black bg-grid-white/[0.2] relative flex flex-col items-center justify-center h-screen">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <div className="transform -translate-y-16">
+          <FloatingDock
+            mobileClassName="translate-y-20"
+            items={links}
+          />
+        </div>
+        <div className="transform -translate-x-28">
+          <Maskot />
+        </div>
+        <div className="w-full">
+          <h2 className="mb-10 text-xl text-center sm:text-5xl dark:text-white text-black w-full">
+            Get Waitlist
+          </h2>
+          <PlaceholdersAndVanishInput
+            placeholders={placeholders}
+            onChange={(e) => setMail(e.target.value)}
+            onSubmit={onSubmit}
+          />
+        </div>
       </div>
-      <div className="transform -translate-x-28"> 
-        <Maskot />
-      </div>
-      <div className="w-full">
-      <h2 className="mb-10 text-xl text-center sm:text-5xl dark:text-white text-black w-full">
-      Get Waitlist
-      </h2>
-      <PlaceholdersAndVanishInput
-        placeholders={placeholders}
-        onChange={handleChange}
-        onSubmit={onSubmit}
-      />
-      </div>
-    </div>
-    
-  );
+
+    );
 }
